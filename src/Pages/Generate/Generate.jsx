05fbc649beeead7809f3cdb156528055a65b88a8
Gurudarshan57 <venom573116@gmail.com>
2024-07-15 23:08:45 +0530

    completed generate page

--- src/Pages/Generate/Generate.jsx
+++ src/Pages/Generate/Generate.jsx
@@ -1,10 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 
 const Generate = () => {
     const [long_url, setLong_url] = useState("")
     const [url_name, setUrl_name] = useState("")
     const [s_url, setS_url] = useState("")
+    const [recentURL, setRecentURL] = useState([])
+
+    useEffect(() => {
+        getRecent()
+    }, [])
+
+    const getRecent = async () => {
+        try {
+            const resp = await fetch(import.meta.env.VITE_Backend + "/s_url/getRecent", {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+                credentials: 'include'
+            })
+            const res = await resp.json()
+            if (resp.status === 200) {
+                setRecentURL(res.data)
+            }
+            else {
+                setRecentURL([])
+            }
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
     const handleCreatesurl = async (e) => {
         e.preventDefault()
         const payload = {
@@ -20,10 +45,10 @@ const Generate = () => {
             })
             const resp = await res.json()
             if (res.status === 200) {
-                toast.success("Short URL successfully created")
+                toast.success(resp.msg)
                 setLong_url("")
                 setUrl_name("")
-                setS_url(import.meta.env.VITE_Backend + "/shorturl/" + resp.msg)
+                setS_url(import.meta.env.VITE_Backend + "/shorturl/" + resp.s_id)
             }
             else {
                 toast.warning(resp.msg);
@@ -41,7 +66,13 @@ const Generate = () => {
                 <button onClick={handleCreatesurl}>Create Short URL</button>
             </form>
             <div>
-                {s_url}
+                <a href={s_url} target='_blank'>{s_url}</a>
+            </div>
+            <div>
+                <h2>Short URL's [{recentURL.length}]</h2>
+                <table> {recentURL.length != 0 ? recentURL.map(ele => {
+                    return <tr><td>{ele.name}</td><td>{ele.short_id}</td><td>{ele.url}</td></tr>
+                }) : <h4>No URL's to display</h4>}</table>
             </div>
         </div>
     )
