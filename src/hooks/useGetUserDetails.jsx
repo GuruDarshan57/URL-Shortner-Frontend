3874c7b782cac3a6a55c3d8c0787b12aecf93dfa
Gurudarshan57 <venom573116@gmail.com>
2024-07-19 13:01:56 +0530

    minor changes

--- src/hooks/useGetUserDetails.jsx
+++ src/hooks/useGetUserDetails.jsx
@@ -3,10 +3,6 @@ import React, { useState } from 'react'
 //custom hook to fetch stored user data
 const useGetUserDetails = () => {
     const [user_data, setUser_data] = useState(JSON.parse(localStorage.getItem("user_data")) || "")
-    if (Date.now() > user_data.expiry) {
-        setUser_data("")
-        localStorage.removeItem(user_data)
-    }
     return { user_data, setUser_data }
 }
 
