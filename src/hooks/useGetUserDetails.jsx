be14bbabd0b028fc5645ea6c71bdcb8f0ebd53fd
Gurudarshan57 <venom573116@gmail.com>
2024-07-25 20:26:45 +0530

    cookie expiry added

--- src/hooks/useGetUserDetails.jsx
+++ src/hooks/useGetUserDetails.jsx
@@ -3,6 +3,11 @@ import React, { useState } from 'react'
 //custom hook to fetch stored user data
 const useGetUserDetails = () => {
     const [user_data, setUser_data] = useState(JSON.parse(localStorage.getItem("user_data")) || "")
+    if (user_data?.exp) {
+        if (Date.now() > user_data.exp) {
+            setUser_data("")
+        }
+    }
     return { user_data, setUser_data }
 }
 
